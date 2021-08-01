936210ac564a99ad59c889979a11455b773a6821
Notchmods <Nomics3000@gmail.com>
2021-08-01 16:41:14 +1000

    The file of the image viewer

---
+++ Imageviewer/Face recognition app/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Face_recognition_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.ShowDialog();
+                openFileDialog1.Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|All files (*.*)|*.*";
+                if(openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    pictureBox1.ImageLocation = openFileDialog1.FileName;
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Incorrect file format");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sf = new SaveFileDialog();
+            sf.Filter = "JPG(*.JPG)|*.jpg";
+            if(sf.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(sf.FileName);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.SelectAll();
+            textBox1.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebRequest wr = WebRequest.Create(textBox1.Text);
+            using (var respond = wr.GetResponse())
+            {
+                using(var str = respond.GetResponseStream())
+                {
+                    pictureBox1.Image = Bitmap.FromStream(str);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+            }
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox1.SelectAll();
+            textBox1.Focus();   
+        }
+    }
+}
